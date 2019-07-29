4b1c53010bbe5076b6f0dce89addf3ac1f2016d8
hsimao <e087754958@gmail.com>
2019-07-29 23:35:27 +0800

    add: 簡易訂閱 realtime data 功能

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 import db from "./db";
 import Query from "./resolvers/Query";
 import Mutation from "./resolvers/Mutation";
+import Subscription from "./resolvers/Subscription";
 import User from "./resolvers/User";
 import Post from "./resolvers/Post";
 import Comment from "./resolvers/Comment";
+
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers: {
     Query,
     Mutation,
+    Subscription,
     User,
     Post,
     Comment
   },
   // 將要通用的變數放置於 content, 之後即可在 resolvers 內透過 ctx 取得
   context: {
-    db
+    db,
+    pubsub
   }
 });
 
