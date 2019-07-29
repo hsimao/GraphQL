298a95045f32bd6fc6486d398f8e0a94506e6780
hsimao <e087754958@gmail.com>
2019-07-30 01:12:39 +0800

    add: createComment 新增訂閱 comment, 即時取得最新資料

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -119,7 +119,7 @@ const Mutation = {
     return post;
   },
 
-  createComment(parent, args, { db }, info) {
+  createComment(parent, args, { db, pubsub }, info) {
     // 檢查用戶是否存在
     const userExists = db.users.some(user => user.id === args.data.author);
 
@@ -142,6 +142,7 @@ const Mutation = {
     };
 
     db.comments.push(comment);
+    pubsub.publish(`comment ${args.data.post}`, { comment });
 
     return comment;
   },
